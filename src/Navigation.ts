4d84804435fed67b89d4e9d0a47c0664f2fc3158
Adam Lorek <lorekadam@interia.pl>
2019-06-13 14:21:47 +0200

    one from given

--- src/Navigation.ts
+++ src/Navigation.ts
@@ -15,7 +15,7 @@ const AppNavigator = createStackNavigator(
     [screens.ONE_FROM_GIVEN]: OneFromGiven,
   },
   {
-    initialRouteName: screens.ONE_FROM_GIVEN,
+    initialRouteName: screens.HOME,
     headerMode: 'none',
   },
 );
--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 import { withNavigation } from 'react-navigation';
 import { IconButton } from 'react-native-paper';
 import { NavigationProps } from '../types';
-import { HOME } from '../screens';
 
 function BackButton(props: NavigationProps) {
-  return <IconButton icon="arrow-back" size={20} onPress={() => props.navigation.navigate(HOME)} />;
+  return <IconButton icon="arrow-back" size={20} onPress={() => props.navigation.goBack()} />;
 }
 
 export default withNavigation(BackButton);
